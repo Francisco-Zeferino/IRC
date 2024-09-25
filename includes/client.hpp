2fd226662bea064a584fea51cf125e6a3d0647d6
Francisco-Zeferino <franciscozeferino06@gmail.com>
2024-09-25 14:57:10 +0100

    Code refactor

--- includes/client.hpp
+++ includes/client.hpp
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   client.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/23 15:01:10 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/09/25 12:28:45 by ffilipe-         ###   ########.fr       */
+/*   Created: 2024/09/25 12:06:53 by ffilipe-          #+#    #+#             */
+/*   Updated: 2024/09/25 12:13:20 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/server.hpp"
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
 
-int main(int argc, char **argv)
-{
-    if(argc != 3){
-        std::cout << "Usage: ./ircserv [port] [password]" << std::endl;
-        return 1;
-    }else{
-        Server server;
-        server.setupServer(argv[1]);
-        return 0;
-    }
-}
+#include <iostream>
+
+class Client{
+    private :
+        std::string username;
+        std::string nickname;
+        std::string password;      
+};
+
+#endif
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -6,45 +6,57 @@
 /*   By: ffilipe- <ffilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 15:53:28 by ffilipe-          #+#    #+#             */
-/*   Updated: 2024/09/24 15:54:05 by ffilipe-         ###   ########.fr       */
+/*   Updated: 2024/09/25 14:54:42 by ffilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/server.hpp"
+#include "../../includes/server.hpp"
 
 Server::Server(){}
 
 Server::~Server(){}
 
-void Server::bindSocket(char *port){
+void Server::setupServer(char *port){
+    _port = port;
+    createSocket();
+    bindSocket();
+    listenSocket();
+    setConnection();
+}
+
+void Server::createSocket(){
     int opt = 1;
-    std::cout << "Binding socket to port " << port << std::endl;
+    std::cout << "Binding socket to port " << _port << std::endl;
     serverSocket = socket(AF_INET, SOCK_STREAM, 0);
     setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(int));
     serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(atoi(port));
+    serverAddr.sin_port = htons(atoi(_port));
     serverAddr.sin_addr.s_addr = INADDR_ANY;
+}
+
+void Server::bindSocket(){
     if(bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0){
         std::cout << "Error binding socket" << std::endl;
         exit(1);
     }
+}
+
+void Server::listenSocket(){
     if(listen(serverSocket, 1024) < 0){
         std::cout << "Error listening on socket" << std::endl;
         exit(1);
     }
+}
+
+void Server::setConnection(){
     size_t serverAddrSize = sizeof(serverAddr);
     if((connection = accept(serverSocket, (struct sockaddr *)&serverAddr, (socklen_t *)&serverAddrSize)) < 0){
         std::cout << "Error accepting connection" << std::endl;
         exit(1);
     }
     while(1){
-        std::string msg;
-        char buffer[1024] = {0};
-        read(connection, buffer, 1024);
-        std::cout << buffer << std::endl;
-        std::getline(std::cin, msg);
-        msg.append("\r\n");
+        char msg[1024] = {0};
+        read(connection, msg, 1024);
         std::cout << msg << std::endl;
-        write(connection, msg.c_str(), msg.length());
     }
 }
